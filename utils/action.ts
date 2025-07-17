c2d8d25c341b65fabebaf54c1d222aca1a92a5bd
maltamari <mo8837145@gmail.com>
2025-07-17 15:05:50 +0300

    Update action.ts

--- utils/action.ts
+++ utils/action.ts
@@ -16,28 +16,6 @@ export async function handleSubmit(
     return { message: `❌ ${messages.join(" | ")}` };
   }
 
-  // ✅ Email Validation via Kickbox
-  const emailToCheck = result.data.email;
-  const kickboxApiKey = process.env.KICKBOX_API_KEY;
-
-  try {
-    const res = await fetch(
-      `https://api.kickbox.com/v2/verify?email=${emailToCheck}&apikey=${kickboxApiKey}`
-    );
-
-    const data = await res.json();
-
-    if (data.result !== "deliverable") {
-      return {
-        message: "❌ Please enter a valid real email address 📧",
-      };
-    }
-  } catch (err) {
-    return {
-      message: "❌ Failed to validate email address.",
-    };
-  }
-
   // ✅ Send email if validation passes
   try {
     const transporter = nodemailer.createTransport({
