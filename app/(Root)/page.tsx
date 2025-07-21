70af9dc55948cf88e5db4710ab46106832b96b78
maltamari <mo8837145@gmail.com>
2025-07-21 15:20:21 +0300

    Update page.tsx

--- app/(Root)/page.tsx
+++ app/(Root)/page.tsx
@@ -3,11 +3,18 @@ import Contents from "@/components/Home/Content";
 import Introduction from "@/components/Home/Introduction";
 import Projects from "@/components/Home/projects";
 import Service from "@/components/Home/Services";
-import { Suspense } from "react";
-
+import { Suspense, useEffect } from "react";
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 export default function Home() {
+    useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true,
+    })
+  }, [])
   return (
-    <>
+   <div  data-aos="fade-up">
       <Introduction />
       
       <Suspense fallback={<p className="text-center">Loading projects...</p>}>
@@ -16,7 +23,7 @@ export default function Home() {
 
       <Service />
       <Contents />
-    </>
+   </div>
   );
 }
 
