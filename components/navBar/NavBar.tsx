888e19619f16169032c871b01b528a2929741ecd
maltamari <mo8837145@gmail.com>
2025-07-21 18:06:42 +0300

    Update NavBar.tsx

--- components/navBar/NavBar.tsx
+++ components/navBar/NavBar.tsx
@@ -7,11 +7,11 @@ import SideBar from './sidebar'
 function NavBar() {
   return (
     <nav className='fixed top-0 left-0 w-full  border-b-1 mb-10 bg-gray-100 dark:bg-gray-950 dark:border-gray-500 z-20  '>
-          <Container className={"w-full flex justify-between py-4 "}>
+          <div className="w-full flex justify-between py-4 px-7 items-center ">
             <Logo/>
             <Pages/>
             <SideBar/>
-          </Container>
+          </div>
     </nav>
   )
 }
