4eced3cc189609f1a9de38610c11bfa4b2273d47
Mohammed Abdallah <mo8837145@gmail.com>
2025-11-19 00:32:35 +0300

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,16 +1,14 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://www.mohammed-labs.site',
-  generateRobotsTxt: true,
+  siteUrl: 'https://www.mohammed-labs.site', // رابط موقعك الرئيسي بدون /
+  generateRobotsTxt: true, // ينشئ robots.txt تلقائياً
   sitemapSize: 7000,
-  exclude: ['/404', '/api/*', '/admin/*'],
-
+  changefreq: 'weekly',
+  priority: 0.7,
+  exclude: ['/404'],
   robotsTxtOptions: {
-    policies: [
-      {
-        userAgent: '*',
-        allow: '/',
-      },
+    additionalSitemaps: [
+      'https://www.mohammed-labs.site/sitemap.xml',
     ],
-  },  
+  },
 };
